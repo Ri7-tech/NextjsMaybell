7b8f915bc27e5983c8c9d430f3030cb8615d7ff9
unknown <riyajpatel54@gmail.com>
2026-05-20 12:58:47 +0530

    Fix small bug

--- src/app/(Admin)/Admin-Panel/page.js
+++ src/app/(Admin)/Admin-Panel/page.js
@@ -1,7 +1,8 @@
 import Image from "next/image";
-import MainLayout from "./Common/MainLayout";
-import Homepage from "./Componentr/Homepage";
-import BestSelling from "./Common/Home page/BestSelling";
+// import MainLayout from "./Common/MainLayout";
+
+// import Homepage from "./Componentr/Homepage";
+// import BestSelling from "./Common/Home page/BestSelling";
 
 export const metadata = {
   title: "Mybell",
